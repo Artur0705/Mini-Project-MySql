0e36fe4e553e66aba6fbc67d55d77c1e8c31f63b
Artur0705 <artlil420@gmail.com>
2022-08-08 21:22:24 +1000

    Seeds data created

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO movies (movie_name)
+
+VALUES ("The Critic"),
+       ("The Nostalgia Critic"),
+       ("Best of the Worst"),
+       ("Ebert & Roeper"),
+       ("Life Itself");
+       
+INSERT INTO reviews (movie_id, review)
+
+VALUES (1, "Jay Sherman is a New York film critic who has to review films he doesn't like for a living."),
+       (2, "A sardonic critic reviews movies and TV shows from his childhood and adulthood."),
+       (3, "Mike, Jay, Rich and the rest of the Red Letter Media crew brave some of the worst movies ever created by man."),
+       (4, "Chicago critics Gene Siskel and Roger Ebert rate new movies with a thumbs up, or a thumbs down."),
+       (5, "The life and career of the renowned film critic and social commentator, Roger Ebert.");
+        
